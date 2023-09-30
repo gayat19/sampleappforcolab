b2778bdf1e91eb07a3356737d85f9ef6592f8ac2
umeshgawande3421 <63260117+umeshgawande3421@users.noreply.github.com>
2023-09-30 10:00:57 +0530

    Update Program.cs

--- SampleSolution/SampleAPI/Program.cs
+++ SampleSolution/SampleAPI/Program.cs
@@ -5,7 +5,7 @@ namespace SampleAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+//added folder 
             // Add services to the container.
 
             builder.Services.AddControllers();
